1d398795323b94b238506029248145803e840123
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-05-08 20:45:23 +0900

    ADD:HEADER,GLOBALSTYLES

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function header() {
+  return <div></div>;
+}
+
+export default header;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GlobalStyles() {
+  return <div></div>;
+}
+
+export default GlobalStyles;
